d24847dea6090cfd2461b3f4b2dd84fbc148d1dc
aditya <adityakulkarni54321@gmail.com>
2024-12-20 15:13:47 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./Pages/Onboarding/Login/Login";
+import { Route, Router, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -10,11 +11,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route element={<ProtectedRoute />}>
+          {/* <Route element={<ProtectedRoute />}>
             <Route element={<FlowSide />}>
               <Route path="dashboard" element={<Dashboard />} />
             </Route>
-          </Route>
+          </Route> */}
         </Routes>
       </Router>
     </div>
