48205726ca8f2122335a8901e32b87fb985b6d7e
Rehan Mujawar <rehanmujawar023@gmail.com>
2025-02-27 01:29:08 +0530

    changes

--- hooks/useBookings.js
+++ hooks/useBookings.js
@@ -1,16 +1,44 @@
+// import { useQuery } from "@tanstack/react-query";
+// import {
+//   fetchAllCompletedBookings,
+//   fetchAllPendingBookings,
+//   handleCompleteBooking,
+// } from "../api/apiService";
+
+// export const useCompletedBookings = () => {
+//   return useQuery("completedBookings", fetchAllCompletedBookings);
+// };
+
+// export const usePendingBookings = () => {
+//   return useQuery("pendingBookings", fetchAllPendingBookings);
+// };
+
+// export const useCompleteBooking = (id) => {
+//   return useQuery({
+//     queryKey: ["completeBooking", id],
+//     queryFn: () => handleCompleteBooking(id),
+//     enabled: false,
+//   });
+// };
 import { useQuery } from "@tanstack/react-query";
 import {
   fetchAllCompletedBookings,
   fetchAllPendingBookings,
-  handleCompleteBooking
+  handleCompleteBooking,
 } from "../api/apiService";
 
 export const useCompletedBookings = () => {
-  return useQuery("completedBookings", fetchAllCompletedBookings);
+  return useQuery({
+    queryKey: "completedBookings",
+    queryFn: fetchAllCompletedBookings,
+  });
 };
 
 export const usePendingBookings = () => {
-  return useQuery("pendingBookings", fetchAllPendingBookings);
+  return useQuery({
+    queryKey: "pendingBookings",
+    queryFn: fetchAllPendingBookings,
+  });
 };
 
 export const useCompleteBooking = (id) => {
